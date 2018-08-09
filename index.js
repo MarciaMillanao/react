2d57fe4a8117fb33d02463256210110f23c02b77
marciamillanao <marcia.millanao@gmail.com>
2018-08-09 17:00:19 -0400

    Agregando datos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // React me servirá para crear mis omponentes
 import React from 'react';
 // ReactDom me servirá para renderizar mis componentes creados
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 // Acá es donde guardaremos en una variable nuestro id de html (eta es la parte de DOM que ya existe)
 const container = document.getElementById('app');
@@ -10,4 +10,5 @@ const container = document.getElementById('app');
 // Lo que voy a renderizar puede ser un componente o un elemento de React
 // Y donde debe ser una parte del DOM que ya exista (index.html)
 const holaMarcia = <h1>Hola Marcia</h1>;
+
 ReactDOM.render(holaMarcia ,container);
