1b5ca120ff63beafc79127c48527fd698fe0c629
marciamillanao <marcia.millanao@gmail.com>
2018-08-10 17:42:01 -0400

    agregando estilos

--- src/playlist/component/media.js
+++ src/playlist/component/media.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-
+import MediaCss from './media.css';
 // Acá crearemos nuestro componente que puede ser funcional, puro o de estado(que este es el mas clasico)
 // Creamos nuestra clase que se llamará Media que va a extender de Component (que ya existe en react)
 class Media extends Component {
   // este componente se instanciará con un método llamado render, que tendrá dentro todo el código html
   render() {
+     // En react podemos utilizar estilos inline, los tenemos que guardar en una variable y luego utilizarla en el elemento que querramos 
+    /*const styles = {
+      //se declara como un objeto y dentro le daremos los estilos que querramos por lo que no se puede utilizar las propiedades en dos palabras front-size, sino que debemos declararlas de la siguiente manera fontSize y las medidas de px debemos solo ponerles en numero
+      container: {
+        color: 'green',
+        cursor:'pointer',
+        width: 260,    
+        border: '1px solid red',
+        textAlign: 'center',
+        backgroundColor: '#fff',
+      }
+    }*/
     // el código html que rendizaremos debe ir dentro de un return
     return (
       //podemos ponerle clases o lo que queramos a nuestro código html, al igual que fuera html comun, pero se llama JSX, que es la forma que se nombra el escribir html dentro de JS
-      <div>
+      // Como la palabra class es reservada en JS, para asignarle una clase a nuestros elementos utilizamos className
+      <div className="Media"> 
         <div>
           <h3> Practicando React</h3>
           <img scr="" alt="" width={260} height={160}/>
