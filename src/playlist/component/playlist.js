cc356846bdb4a3c4a07c734b37f3294ab10f686e
marciamillanao <marcia.millanao@gmail.com>
2018-08-16 16:37:36 -0300

    actualizaciones

--- src/playlist/component/playlist.js
+++ src/playlist/component/playlist.js
@@ -6,8 +6,10 @@ class Playlist extends Component {
   // Al utilizar render debemos recordar que debe retornar algo
   render() {
     // Quiero que en mi variable me guarde desde la data mi elemento categories, me menciono que posoción y luego el lugar
-    const playlist = this.props.data.categories[0].playlist
+    const playlist = this.props.data.categories[1].playlist
     console.log(this.props.data);
+    console.log(playlist);
+
     // Dentro de mi return yo le puedo pedir cualquier cosa, como mi ejemplo es un array yo puedo utilizar cualquier método que me sirva para este, en este caso map, así por cada elemento de mi lista devolveré (renderizaré Media), cuando haga el recorrido yo le puede decir que quiero que me devuelva, en este caso un titulo, al agregarle un key, nos permite que cada elemento devuelto tenga su propio key 
     // al poner ...item yo le estoy pasando toda la información de json 
     return (
