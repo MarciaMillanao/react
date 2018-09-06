464fb6c104ef7069343a76dca8dac672aaf6d429
marciamillanao <marcia.millanao@gmail.com>
2018-09-06 09:46:52 -0300

    agregando boton cerrar a modal

--- src/pages/containers/container.js
+++ src/pages/containers/container.js
@@ -5,17 +5,33 @@ import Realated from '../components/realated';
 import ModalContainer from '../../widgets/containers/modalcontainer';
 import Modal from '../../widgets/components/modal';
 
+// en nuestro evento handleClick, podemos poderle el nombre que nosotros queramos, mas descriptivo así nos permitirá que entendamos de mejor forma nuestro código, ademas que este nombre será el que tenga nuestra función(que la declararemos en es7)
 class Home extends Component {
+  // esta sera nuestra funcion en ES7 es menos código, más ordenado, de esta forma this esta disponible dentro para poder utilizarlo, así nos evitamos de llamar tantas veces un evento
+  // dentro de nuestra const, dejamos mi modal visible en false, debo condicionar mi conponente Modal, en la parte de mi código donde lo renderizo, EC6 nos permite hacerlo de forma muy fácil, si no es necesario tener un if y un else, simpletente lo hago con un ternario ej. { this.state.modalVisible && }, para que esto nos funciones, adicionalmente debo inicializar mi modal y así poder realizar cambio de estado
+  state = {
+    modalVisible: true, //porque verdadero, porque se verá en iniciar nuestra página
+  }
+  handleModalCloseClick = (event) => {
+    this.setState({
+      modalVisible: false,
+    })
+  }
   render(){
     return (
       <HomeLayout>
         <Realated />
         <Categories categories={this.props.data.categories} />
-        <ModalContainer>
-          <Modal >
-            <h1>Hola Marcia </h1>
-          </Modal>
-        </ModalContainer>
+        { 
+          this.state.modalVisible && 
+          <ModalContainer>
+            <Modal 
+              handleClick={this.handleModalCloseClick}
+              >
+              <h1>Hola Marcia </h1>
+            </Modal>
+          </ModalContainer>
+        }
       </HomeLayout>
     )
   }
--- src/widgets/components/modal.js
+++ src/widgets/components/modal.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './modal.css';
 
+// crearemos un boton para cerrar nuestro modal, lo haremos dentro de nuestro div para que este condicionado siempre que aparezca cuando este visible nuestro div modal
+// A el boton le daremos una evento para cerrarlo que luego en nuestra archivo container.js en nuestro contenedor le daremos esta propiedad
 function Modal(props) {
   return (
     <div className="Modal">
       Esto es un saludo
       {props.children}
+      <button onClick={props.handleClick}> Cerrar </button>
     </div>
   )
 }
